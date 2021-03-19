aa6517c789757db230a4e9ce231f51bc7477100f
Lassiter Brown <lassiterbrown@quickenloans.com>
2021-03-19 09:56:52 -0400

    First commit of game

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SimpleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You wake up and this is what you see. What would you like to do?");
+            Console.Write("\n\n      o    O                     o   O   o o       ");
+            Console.Write("\n      |  Oo| o                   |oO |   |O|       ");
+            Console.Write("\n     o| o|||o|                  o|||o| o |||o      ");
+            Console.Write("\n     \\|o||/||/                  \\||/|| | |\\|/      ");
+            Console.Write("\n      |/\\| |/                    |/ \\| | | |       ");
+            Console.Write("\n      |  | /                      \\  \\ | / /       ");
+            Console.Write("\n      \\  |/                        \\  \\|/ /        ");
+            Console.Write("\n       \\ |                          \\  | /         ");
+            Console.Write("\n        \\|                           \\ |/          ");
+            Console.Write("\n         |                            \\|           ");
+            Console.Write("\n         |                             |           ");
+            Console.Write("\n         |                             |           ");
+            Console.Write("\n_________|_____________________________|___________");
+            Console.Write("\n                \\            \\                     ");
+            Console.Write("\n                 \\            \\                    ");
+            Console.Write("\n                  \\            \\                   ");
+            Console.Write("\n                  |            |                  ");
+            Console.Write("\n                  |            |                  ");
+            Console.Write("\n                 /            /                  ");
+            Console.Write("\n                /            /                   ");
+            Console.Write("\n...................................................");
+        }
+    }
+}
